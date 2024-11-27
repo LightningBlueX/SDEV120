254cbd4407bc7de0bf08fccd5a868cb496633553
Mark McNew <mmcnew@ivytech.edu>
2024-11-26 20:34:44 -0500

    Add files via upload

---
+++ First Python IDLE script file Cats and Dogs - Mark M.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("I just wrote my first Python Program")
+print("This is so much fun")
+
+# Prompt the user for their name and store it in the variable 'name'
+
+name = input("Hello, what is your name? ")
+print("Welcome, it is so nice to meet you, " + name)
+
+#prints a message with the user's name
+
+# This program sets variables for a sentence and prints it to the screen.
+# Variable to hold the word "cats"
+c = "cats"
+
+# Variable to hold the word "dogs"
+d = "dogs"
+
+# Variable to hold the complete sentence
+s = "It is raining " + c + " and " + d + "."
+
+# Print the sentence to the screen
+print(s)
