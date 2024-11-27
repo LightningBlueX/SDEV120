2c8619c7ca93540e46836d0b6a6fea2d8e591ddc
Mark McNew <mmcnew@ivytech.edu>
2024-11-26 20:35:51 -0500

    Add files via upload

---
+++ Pseudocode MarkMcNew.py
@@ -0,0 +1,36 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Pseudocode Exercise 1
+SyntaxError: invalid syntax
+>>> 
+... start
+...     input MilesTraveled
+...     input GallonsOfGasUsed
+...     MilesPerGallon = MilesTraveled / GallonsOfGasUsed
+...     output MilesPerGallon
+... stop
+... 
+... Exercise 2
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> start
+...      input rent
+...      costPerDay = rent / 30
+...      output rent
+... stop
+... 
+SyntaxError: multiple statements found while compiling a single statement
+>>> Exercise 3
+SyntaxError: invalid syntax
+>>> 
+>>> start
+...      input pay
+...      input rent
+...      input utilities
+...      input groceries
+...      totalBills  = rent + utlilities + groceries
+...      discretionary = pay – totalBills
+...      output pay
+...      output totalBills 
+...      output discretionary
+... stop
